530d3eb3c6395a0a796e038dce1d8744942a6554
Sala3A-DS <sala3a.ds.curso@gmail.com>
2026-05-08 20:19:10 -0300

    Update RetrofitClient.kt

--- app/src/main/java/com/example/agend/auth/RetrofitClient.kt
+++ app/src/main/java/com/example/agend/auth/RetrofitClient.kt
@@ -1,22 +1,31 @@
 package com.example.agend.auth
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-
-    // Se o seu Back-end está rodando no IntelliJ (sua máquina), o emulador do Android
-    // precisa deste IP especial (10.0.2.2) para acessar o localhost.
-    // Quando você for para a nuvem, você trocará isso pela URL do Railway.
     private const val BASE_URL = "https://dev-mobile-back.onrender.com/"
+    
+    // Variável para guardar o token na memória (ou carregar do SharedPreferences)
+    var token: String? = null
 
-    // Aqui nós "criamos o objeto" do Retrofit com a configuração base
+    private val client = OkHttpClient.Builder().addInterceptor { chain ->
+        val requestBuilder = chain.request().newBuilder()
+        
+        // Se tivermos um token, adicionamos no cabeçalho de todas as chamadas
+        token?.let {
+            requestBuilder.addHeader("Authorization", "Bearer $it")
+        }
+        
+        chain.proceed(requestBuilder.build())
+    }.build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        // O Gson é quem converte magicamente as Kotlin Data Classes em texto JSON
+        .client(client) // Adicionamos o cliente com suporte a Token
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    // Essa variável 'api' é a que você vai usar nas telas do seu App para chamar os métodos
     val api: AuthApi = retrofit.create(AuthApi::class.java)
 }
